b063345343270b554d448ee95daef59496f325ae
Tim Tully <tully_tim@yahoo.com>
2023-02-16 18:11:56 -0800

    latest

--- pinecli.py
+++ pinecli.py
@@ -86,33 +86,36 @@ def _pinecone_init(apikey, environment):
     environment = os.environ.get('PINECONE_ENVIRONMENT', environment)
     pinecone.init(api_key=apikey, environment=environment)
 
+
 def _format_values(vals):
     return ",".join(str(x) for x in vals)[:30]
-    
+
+
 def _print_table(res, pinecone_index_name, namespace, include_meta, include_values, expand_meta):
-    table = Table(title=f"🌲 {pinecone_index_name} ns=({namespace}) Index Results")
+    table = Table(
+        title=f"🌲 {pinecone_index_name} ns=({namespace}) Index Results")
     table.add_column("ID", justify="right", style="cyan", no_wrap=True)
-    if(include_values):
+    if (include_values):
         table.add_column("Values", style="magenta")
-    if(include_meta):
+    if (include_meta):
         table.add_column("Meta", justify="right", style="green")
     table.add_column("Score", justify="right", style="green")
 
     for row in res.matches:
-        metadata=''
-        if(include_meta):
+        metadata = ''
+        if include_meta and 'metadata' in row:
             metadata = str(row['metadata'])
             metadata = metadata[:200] if not expand_meta else metadata
+            
         if include_values and include_meta:
-            table.add_row(row['id'], _format_values(row['values']), metadata, str(row['score']))
+            table.add_row(row['id'], _format_values(
+                row['values']), metadata, str(row['score']))
         elif include_values and not include_meta:
-            table.add_row(row['id'], _format_values(row['values']), str(row['score']))
+            table.add_row(row['id'], _format_values(
+                row['values']), str(row['score']))
         else:
-            table.add_row(row['id'], str(row['score']))
+            table.add_row(row['id'], metadata, str(row['score']))
 
-        #table.add_row(row['id'], metadata, str(row['score']))
-        # print(row['values'])
-        # table.add_row(row['id'], "".join(x for x in row['values']), str(row['score']))
     console = Console()
     console.print(table)
 
@@ -136,12 +139,13 @@ def query(pinecone_index_name, apikey, query_vector, region, topk, include_value
     query_vector = [random.random() for i in range(1536)]
     res = pinecone_index.query(query_vector, top_k=topk, include_metadata=True,
                                include_values=include_values, namespace=namespace)
-    
+
     if print_table:
-        _print_table(res, pinecone_index_name, namespace, include_meta, include_values, expand_meta)
+        _print_table(res, pinecone_index_name, namespace,
+                     include_meta, include_values, expand_meta)
     else:
         print(res)
-        
+
     if show_tsne:
         show_tsne_plot(res.matches)
 
@@ -195,6 +199,7 @@ def fetch(pinecone_index_name, apikey, region, vector_ids, namespace, pretty):
     else:
         print(fetch_response)
 
+
 @click.command(short_help='Extracts text from url arg, vectorizes w/ openai embedding api, and upserts to Pinecone.')
 @click.option('--apikey', help='Pinecone API Key')
 @click.option('--region', help='Pinecone Index Region', show_default=True, default=default_region)
@@ -206,7 +211,7 @@ def upsert(pinecone_index_name, apikey, region, vector_literal, namespace, debug
     """ 
     Upserts vectors into the index <PINECONE_INDEX_NAME> by using the <VECTOR_LITERAL> which is a string representation of a list of tuples.
     Note the literal is quoted.
-    
+
     \b
     Example:
     % ./pinecli.py upsert upsertfile "[('vec1', [0.1, 0.2, 0.3], {'genre': 'drama'}), ('vec2', [0.2, 0.3, 0.4], {'genre': 'action'}),]" --debug 
@@ -218,7 +223,8 @@ def upsert(pinecone_index_name, apikey, region, vector_literal, namespace, debug
     resp = index.upsert(vectors=eval(vector_literal), namespace=namespace)
     if debug:
         print(resp)
-        
+
+
 @click.command(short_help='Updates the index based on the given id passed in.')
 @click.option('--apikey', help='Pinecone API Key')
 @click.option('--region', help='Pinecone Index Region', show_default=True, default=default_region)
@@ -232,8 +238,14 @@ def update(pinecone_index_name, apikey, region, id, vector_literal, metadata, na
     """ Updates the index <PINECONE_INDEX_NAME> with id <ID> and vector values <VECTOR_LITERAL> """
     _pinecone_init(apikey, region)
     index = pinecone.Index(pinecone_index_name)
-    resp = index.update(id=id, values=eval(vector_literal), set_metadata=eval(metadata), namespace=namespace)
-    
+    if metadata:
+        resp = index.update(id=id, values=eval(vector_literal), set_metadata=eval(metadata), namespace=namespace)
+    else:
+        resp = index.update(id=id, values=eval(vector_literal), namespace=namespace)
+
+    print(resp)
+
+
 @click.command(short_help='Extracts text from url arg, vectorizes w/ openai embedding api, and upserts to Pinecone.')
 @click.option('--apikey', help='Pinecone API Key')
 @click.option('--openaiapikey', required=True, help='OpenAI API Key')
@@ -286,32 +298,36 @@ def upsert_webpage(pinecone_index_name, apikey, openaiapikey, region, url, debug
         rv = pinecone_index.upsert(vectors=to_upsert)
         print(rv)
 
+
 @click.command(short_help='Shows a preview of vectors in the <PINECONE_INDEX_NAME>')
 @click.option('--apikey', help='Pinecone API Key')
 @click.option('--region', help='Pinecone Index Region', default=default_region)
 @click.option('--numrows', default=10, help='The number of rows to show')
 @click.option('--random_dims', is_flag=True, help='Flag to have query vector dims be random.  Default will be 0.0')
-@click.option('--include_values', help='Should we return the vectors', show_default=True, default=True)
+@click.option('--include-values', help='Should we return the vectors', show_default=True, default=True)
 @click.option('--namespace',  default="", help='Namespace to select results from')
 @click.option('--include-meta', help='Whether to include the metadata values', default=False, show_default=True)
 @click.option('--expand-meta', help='Whether to fully expand the metadata returned.', is_flag=True, show_default=True, default=False)
 @click.option('--print-table', help='Display the output as a pretty table.', is_flag=True, show_default=True, default=False)
-@click.argument('pinecone_index_name')       
+@click.argument('pinecone_index_name')
 def head(pinecone_index_name, apikey, region, numrows, random_dims, namespace, include_meta, expand_meta, include_values, print_table):
     """ Shows a preview of vectors in the <PINECONE_INDEX_NAME> with optional numrows (default 10) """
     _pinecone_init(apikey, region)
     index = pinecone.Index(pinecone_index_name)
     res = index.describe_index_stats()
     dims = res['dimension']
-    if(random_dims):
+    if (random_dims):
         dims = [random.random() for _ in range(dims)]
     else:
         dims = [0.0 for _ in range(dims)]
-    resp = index.query(vector=dims, top_k=numrows, namespace=namespace, include_metadata=include_meta, include_values=include_values)
+    resp = index.query(vector=dims, top_k=numrows, namespace=namespace,
+                       include_metadata=include_meta, include_values=include_values)
     if print_table:
-        _print_table(resp, pinecone_index_name, namespace, include_meta, include_values, expand_meta)
+        _print_table(resp, pinecone_index_name, namespace,
+                     include_meta, include_values, expand_meta)
     else:
         print(resp)
+
 
 @click.command(short_help='Creates a Pinecone Index.')
 @click.option('--apikey', help='Pinecone API Key')
@@ -401,10 +417,10 @@ def upsert_file(pinecone_index_name, apikey, region, vector_file, batch_size, na
     # for chunk in pd.read_csv(vector_file, chunksize=batch_size, dtype={'ID':str, 'Vectors':str}):
 
     for chunk in pd.read_csv(vector_file, chunksize=batch_size, converters={'Vectors': convert}):
-        #print(chunk["Vectors"])
-        #print(chunk)
-        #print(chunk.to_records(index=False).tolist())
-        #print(chunk.apply(tuple, axis=0))
+        # print(chunk["Vectors"])
+        # print(chunk)
+        # print(chunk.to_records(index=False).tolist())
+        # print(chunk.apply(tuple, axis=0))
         print("-----")
         print([tuple(chunk['foo']), tuple(chunk['Vectors'])])
         # index.upsert(vectors=chunk.to_records(index=False).tolist(), namespace=namespace)
