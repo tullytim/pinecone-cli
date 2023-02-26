2978b076dc840627c812ac3c411ee567cfbaa2ce
Tim Tully <tully_tim@yahoo.com>
2023-02-26 09:44:54 -0800

    update deps

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     py_modules=['pinecli'],
     install_requires=[
         'Click', 'pandas', 'numpy', 'openai', 'pinecone-client', 'pinecone-client[grpc]', 'matplotlib', \
-            'scikit-learn', 'beautifulsoup4', 'nltk', 'sklearn', 'rich'
+            'scikit-learn', 'beautifulsoup4', 'nltk', 'sklearn', 'rich', 'ssl', 'dotenv'
     ],
     entry_points={
         'console_scripts': [
