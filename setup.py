05975f91230ec9bdf9ce88d350aa381a3c7098bd
Tim Tully <tully_tim@yahoo.com>
2023-03-24 17:09:34 -0600

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='pinecone-cli is a command-line client for interacting with the pinecone vector embedding database.',
     long_description=open('README.md', 'r').read(),
     long_description_content_type="text/markdown",
-    version='0.5.0',
+    version='0.5.1',
     url='https://github.com/tullytim/pinecone-cli',
     author='Tim Tully',
     author_email='tim@menlovc.com',
