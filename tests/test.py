017ec879efe36e4f9b98735eb3c8e0d501793c96
Tim Tully <tully_tim@yahoo.com>
2023-05-02 22:57:23 -0700

    test

--- tests/test.py
+++ tests/test.py
@@ -127,7 +127,7 @@ class TestPineconeCLI(unittest.TestCase):
     def test_upsert_webpage(self):
         openaiapikey = os.environ['OPENAI_API_KEY']
         stats = self._run([f'{self.cli}', 'upsert-webpage', 'https://www.menlovc.com', 'pageuploadtest', f'--openaiapikey="{openaiapikey}"'])
-        print(f'KEY IS OF LENGTH: {len(openaiapikey)}')
+        print(f'KEY IS OF LENGTH: {len(openaiapikey)}') 
         self.assertIsNotNone(stats)
 
     def test_fetch(self):
